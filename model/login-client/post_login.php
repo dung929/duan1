92258698c2b5496c617ab2ec0a31e44d742d562e
dung929 <duydung929@gmail.com>
2022-11-14 00:57:41 +0700

    layout

---
+++ model/login-client/post_login.php
@@ -0,0 +1,29 @@
+<?php
+require_once('../controller/db.php');
+$user = $_POST['username'];
+$password = $_POST['password'];
+$errors = '';
+if ($user == '') {
+    $errors = $errors . 'User không được để trống!.';
+}
+if ($password == '') {
+    $errors .= 'Password không được để trống!.';
+}
+if ($errors != '') {
+    header("location: login.php?errors=$errors");
+} else {
+    $sql = "SELECT * FROM client WHERE username='$user'";
+    $statement = $connect->prepare($sql);
+    $statement->execute();
+    $loginUser = $statement->fetch();
+    if ($loginUser == false) {
+        $errors = 'Người dùng không tồn tại!';
+        
+        header("location: login.php?errors=$errors");
+    } else if (password_verify($password, $loginUser['password']) ==false) {
+        $errors = 'Mật khẩu không chính xác!';
+        header("location: login.php?errors=$errors");
+    } else {
+        header("location:index.php");
+    }
+}
---
+++ view/index.php
@@ -0,0 +1,3 @@
+<?php include "header.php";?>
+<?php include "banner.php";?>
+<?php include "footer.php";?>
