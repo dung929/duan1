e22b41509edbe58b0581e37ae03f49c9759bf0e7
dung929 <duydung929@gmail.com>
2022-11-14 09:46:23 +0700

    update-2

--- view/login-user/login.php
+++ view/login-user/login.php
@@ -4,37 +4,64 @@ $errors = isset($_GET['errors']) ? $_GET['errors'] : '';
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8">
-    <title>Login admin</title>
-    <link rel="stylesheet" href="styleadm.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login User</title>
+  <link rel="stylesheet" href="./style/login.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+  integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous" />
 </head>
-
 <body>
-    <div class="">
-        <form autocomplete="off" action="../../model/login-user/post_login.php" method="POST">
-            <h2>Sign in</h2>
-            <div class="">
-                <span>Userame</span>
-                <input type="text" required="required" name="username">
-                <i></i>
-            </div>
-            <div class="">
-                <span>Password</span>
-                <input type="password" required="required" name="password">
-                <i></i>
-            </div>
-            <div class="">
-                <a href="">Đăng ký</a>
-                <a href="">Đổi mật khẩu</a>
-            </div>
-            <input type="submit" value="Login">
-        </form>
-    </div>
-    <div style="color: orange   ;">
+<div class="container" id="container">
+	<div class="form-container sign-up-container">
+		<form action="#">
+			<h1>Create Account</h1>
+			<div class="social-container">
+				<a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+				<a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+				<a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+			</div>
+			<span>or use your email for registration</span>
+			<input type="text" placeholder="Name" name="username" />
+			<input type="password" placeholder="Password" name="password" />
+			<button>Sign Up</button>
+		</form>
+	</div>
+	<div class="form-container sign-in-container">
+		<form action="../../model/login-user/post_login.php">
+			<h1>Sign in</h1>
+			<div class="social-container">
+				<a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+				<a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+				<a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+			</div>
+			<span>or use your account</span>
+			<input type="email" placeholder="Name" name="username" />
+			<input type="password" placeholder="Password" name="password" />
+			<a href="#">Forgot your password?</a>
+			<button>Sign In</button>
+		</form>
+	</div>
+	<div class="overlay-container">
+		<div class="overlay">
+			<div class="overlay-panel overlay-left">
+				<h1>Welcome Back!</h1>
+				<p>To keep connected with us please login with your personal info</p>
+				<button class="ghost" id="signIn">Sign In</button>
+			</div>
+			<div class="overlay-panel overlay-right">
+				<h1>Hello, Friend!</h1>
+				<p>Enter your personal details and start journey with us</p>
+				<button class="ghost" id="signUp">Sign Up</button>
+			</div>
+		</div>
+	</div>
+</div>
+<script src="./js/login.js"></script>
+<div style="color: orange   ;">
         <?php echo $errors ?>
     </div>
 </body>
-
 </html>
